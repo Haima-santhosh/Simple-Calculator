53f86291a39d6932023cc46181fb8dd703ee5881
Haima-santhosh <hymasanthosh14@gmail.com>
2025-05-27 12:39:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function add() {
+  let number1 = parseFloat(document.getElementById("number1").value);
+  let number2 = parseFloat(document.getElementById("number2").value);
+  document.getElementById("result").innerText =
+    "Result : " + (number1 + number2);
+}
+function subtract() {
+  let number1 = parseFloat(document.getElementById("number1").value);
+  let number2 = parseFloat(document.getElementById("number2").value);
+  document.getElementById("result").innerText =
+    "Result : " + (number1 - number2);
+}
+function multiply() {
+  let number1 = parseFloat(document.getElementById("number1").value);
+  let number2 = parseFloat(document.getElementById("number2").value);
+  document.getElementById("result").innerText = "Result : " + number1 * number2;
+}
+function divide() {
+  let number1 = parseFloat(document.getElementById("number1").value);
+  let number2 = parseFloat(document.getElementById("number2").value);
+  if (number1 == 0) {
+    document.getElementById("result").innerText = "Can not divide by 0";
+    return;
+  }
+  document.getElementById("result").innerText = "Result : " + number1 / number2;
+}
+function square() {
+  let number1 = parseFloat(document.getElementById("number1").value);
+
+  document.getElementById("result").innerText = "Result : " + number1 * number1;
+}
+function cube() {
+  let number1 = parseFloat(document.getElementById("number1").value);
+
+  document.getElementById("result").innerText =
+    "Result : " + number1 * number1 * number1;
+}
